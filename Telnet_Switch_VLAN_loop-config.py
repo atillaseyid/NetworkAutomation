663c88a319d567387987f6511ac7c061cb448450
atillaseyid <atillaseyid87@gmail.com>
2022-04-26 15:38:57 +0200

    update

--- Telnet_Switch_VLAN_loop-config.py
+++ Telnet_Switch_VLAN_loop-config.py
@@ -1,5 +1,6 @@
 import getpass
 import telnetlib
+from xml.etree.ElementTree import Comment
 
 HOST = "192.168.0.2"
 user = input("Enter your telnet username: ")
@@ -17,7 +18,8 @@ tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
 
-for n in range(2,100):
+#vlan range(2,100)
+for n in range(2,100):  
     tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
     tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
 
